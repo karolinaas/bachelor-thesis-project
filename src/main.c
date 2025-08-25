419ad4005aeb105e89a49b81353acd1a4cb1c470
Karolina Sebestova <karolina.sebestova@nxp.com>
2025-08-25 14:21:52 +0200

    Remove unused DMIC functionality

--- src/main.c
+++ src/main.c
@@ -1,30 +1,23 @@
 /*
- * Copyright 2024-2025 NXP
+ * Copyright 2025 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
-#include <zephyr/audio/dmic.h>
 #include <zephyr/drivers/i2s.h>
 #include <zephyr/audio/codec.h>
 #include <string.h>
 
-#ifndef CONFIG_USE_DMIC
 #include "sine.h"
-#endif
 
 #define I2S_CODEC_TX DT_ALIAS(i2s_codec_tx)
 
 #define SAMPLE_FREQUENCY CONFIG_SAMPLE_FREQ
 #define SAMPLE_BIT_WIDTH (16U)
 #define BYTES_PER_SAMPLE sizeof(int16_t)
-#if CONFIG_USE_DMIC
-#define NUMBER_OF_CHANNELS CONFIG_DMIC_CHANNELS
-#else
 #define NUMBER_OF_CHANNELS (2U)
-#endif
 /* Such block length provides an echo with the delay of 100 ms. */
 #define SAMPLES_PER_BLOCK ((SAMPLE_FREQUENCY / 10) * NUMBER_OF_CHANNELS)
 #define INITIAL_BLOCKS    CONFIG_I2S_INIT_BUFFERS
@@ -63,44 +56,12 @@ static bool trigger_command(const struct device *i2s_dev_codec, enum i2s_trigger
 int main(void)
 {
 	const struct device *const i2s_dev_codec = DEVICE_DT_GET(I2S_CODEC_TX);
-#if CONFIG_USE_DMIC
-	const struct device *const dmic_dev = DEVICE_DT_GET(DT_NODELABEL(dmic_dev));
-#endif
 	const struct device *const codec_dev = DEVICE_DT_GET(DT_NODELABEL(audio_codec));
 	struct i2s_config config;
 	struct audio_codec_cfg audio_cfg;
 	int ret = 0;
 
-#if CONFIG_USE_DMIC
-	struct pcm_stream_cfg stream = {
-		.pcm_width = SAMPLE_BIT_WIDTH,
-		.mem_slab = &mem_slab,
-	};
-	struct dmic_cfg cfg = {
-		.io = {
-			/* These fields can be used to limit the PDM clock
-			 * configurations that the driver is allowed to use
-			 * to those supported by the microphone.
-			 */
-			.min_pdm_clk_freq = 1000000,
-			.max_pdm_clk_freq = 3500000,
-			.min_pdm_clk_dc   = 40,
-			.max_pdm_clk_dc   = 60,
-		},
-		.streams = &stream,
-		.channel = {
-			.req_num_streams = 1,
-		},
-	};
-#endif
-	printk("codec sample\n");
-
-#if CONFIG_USE_DMIC
-	if (!device_is_ready(dmic_dev)) {
-		printk("%s is not ready", dmic_dev->name);
-		return 0;
-	}
-#endif
+	printk("MIDI Synth sample\n");
 
 	if (!device_is_ready(i2s_dev_codec)) {
 		printk("%s is not ready\n", i2s_dev_codec->name);
@@ -123,23 +84,6 @@ int main(void)
 	audio_codec_configure(codec_dev, &audio_cfg);
 	k_msleep(1000);
 
-#if CONFIG_USE_DMIC
-	cfg.channel.req_num_chan = 2;
-	cfg.channel.req_chan_map_lo = dmic_build_channel_map(0, 0, PDM_CHAN_LEFT) |
-				      dmic_build_channel_map(1, 0, PDM_CHAN_RIGHT);
-	cfg.streams[0].pcm_rate = SAMPLE_FREQUENCY;
-	cfg.streams[0].block_size = BLOCK_SIZE;
-
-	printk("PCM output rate: %u, channels: %u\n", cfg.streams[0].pcm_rate,
-	       cfg.channel.req_num_chan);
-
-	ret = dmic_configure(dmic_dev, &cfg);
-	if (ret < 0) {
-		printk("Failed to configure the driver: %d", ret);
-		return ret;
-	}
-#endif
-
 	config.word_size = SAMPLE_BIT_WIDTH;
 	config.channels = NUMBER_OF_CHANNELS;
 	config.format = I2S_FMT_DATA_FORMAT_I2S;
@@ -156,35 +100,19 @@ int main(void)
 	printk("start streams\n");
 	for (;;) {
 		bool started = false;
-#if CONFIG_USE_DMIC
-		ret = dmic_trigger(dmic_dev, DMIC_TRIGGER_START);
-		if (ret < 0) {
-			printk("START trigger failed: %d", ret);
-			return ret;
-		}
-#endif
+
 		while (1) {
 			void *mem_block;
 			uint32_t block_size = BLOCK_SIZE;
 			int i;
 
 			for (i = 0; i < 2; i++) {
-#if CONFIG_USE_DMIC
-				/* If using DMIC, use a buffer (memory slab) from dmic_read */
-				ret = dmic_read(dmic_dev, 0, &mem_block, &block_size, TIMEOUT);
-				if (ret < 0) {
-					printk("read failed: %d", ret);
-					break;
-				}
-
-				ret = i2s_write(i2s_dev_codec, mem_block, block_size);
-#else
 				/* If not using DMIC, play a sine wave 440Hz */
 				mem_block = (void *)&__16kHz16bit_stereo_sine_pcm;
 				block_size = __16kHz16bit_stereo_sine_pcm_len;
 
 				ret = i2s_buf_write(i2s_dev_codec, mem_block, block_size);
-#endif
+
 				if (ret < 0) {
 					printk("Failed to write data: %d\n", ret);
 					break;
@@ -203,13 +131,7 @@ int main(void)
 			printk("Send I2S trigger DRAIN failed: %d", ret);
 			return 0;
 		}
-#if CONFIG_USE_DMIC
-		ret = dmic_trigger(dmic_dev, DMIC_TRIGGER_STOP);
-		if (ret < 0) {
-			printk("STOP trigger failed: %d", ret);
-			return 0;
-		}
-#endif
+
 		printk("Streams stopped\n");
 		return 0;
 	}
